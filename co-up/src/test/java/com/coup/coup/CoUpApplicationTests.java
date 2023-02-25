0fdd37c32dafe7c3ca777709c8f02cf372af7a87
eunzkwon <eunjiii@hotmail.com>
2023-02-25 15:54:22 +0900

    initial setting - connecting to H2 database

---
+++ co-up/src/test/java/com/coup/coup/CoUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coup.coup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
