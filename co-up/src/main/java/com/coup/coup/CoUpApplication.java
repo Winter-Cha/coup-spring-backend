0c7edf59e047e165773f204e5efc52757d83041b
eunzkwon <eunjiii@hotmail.com>
2023-02-25 15:54:22 +0900

    initial setting - connecting to H2 database

---
+++ co-up/src/main/java/com/coup/coup/CoUpApplication.java
@@ -0,0 +1,13 @@
+package com.coup.coup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoUpApplication.class, args);
+	}
+
+}
