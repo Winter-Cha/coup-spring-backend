749e419c9abdacc7f437a0460a6a56c730fe2584
eunzkwon <eunjiii@hotmail.com>
2023-04-20 08:28:04 +0900

    컨트롤러 추가 및 회원등록,조회 개발

--- src/main/java/com/coup/repository/MemberRepository.java
+++ src/main/java/com/coup/repository/MemberRepository.java
@@ -1,29 +1,40 @@
-package com.coup.member;
+package com.coup.repository;
 
+import com.coup.domain.Member;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
 
-    @PersistenceContext
+    //@PersistenceContext
     // spring boot 가 Entity Manager 주입 해줌
     // implementation 'org.springframework.boot:spring-boot-starter-data-jpa'
     // build.gradle 에 이거 등록할 때 em 생성하는게 자동으로 들어감. 설정파일 읽어서 코드 생성 다 해줌
-    private EntityManager em;
+    //private EntityManager em;
+
+    // Repository 도 생성자 injection으로 바꿔줄 수 있음. 스프링부트에서 @Autowired 를 지원하기 때문.
+    private final EntityManager em;
 
     public Long save(Member member) {
-        em.persist(member);
+        em.persist(member); // insert 문이 안나감. commit 될 때 flush가 되면서 insert문이 나가기 떄문
 
         return member.getId();
     }
 
-    public Member find(Long id) {
+    public Member findOne(Long id) {
         return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
     }
 
     public List<Member> findByUserName(String userName) {
---
+++ src/main/java/com/coup/service/MemberService.java
@@ -0,0 +1,67 @@
+package com.coup.service;
+
+import com.coup.domain.Member;
+import com.coup.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+//@Transactional(readOnly = true)
+@RequiredArgsConstructor // final 필드만 가지고 생성자를 만들어 줌. 가장 최선
+public class MemberService {
+
+    // Test 케이스 작성할 때 유리하기 때문에 생성자 injection 사용
+    private final MemberRepository memberRepository;
+
+    /**
+     * 회원 가입
+     * return Member.Id
+     */
+    @Transactional
+    public Long join(Member member) {
+
+        validateDuplicateMember(member); //중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    /**
+     * 중복회원 검증
+     * */
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByUserName(member.getUsername());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    /**
+     * 회원 전체 조회
+     * */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    /**
+     * Id 로 회원 조회
+     * */
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+
+    /**
+     * 회원 가입
+     * return Member
+     */
+    @Transactional
+    public Member createMember(Member member) {
+        validateDuplicateMember(member); //중복 회원 검증
+        memberRepository.save(member);
+        Long memberId = member.getId();
+        return memberRepository.findOne(memberId);
+    }
+
+}
